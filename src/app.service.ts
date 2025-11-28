41e63161f51edaea57a740445bec2c3eb794c3f9
Mario Gómez <mariogomezarr@gmail.com>
2025-11-28 11:20:39 -0500

    update to test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World.';
+    let string = 'Hello World!';
+    return string;
   }
 }
